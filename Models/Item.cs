e5166064287dc59897bbde597ef190b153cc626c
Saksham <fermi4676.e@gmail.com>
2024-06-23 14:58:29 +0545

    Added Comments

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,7 +1,10 @@
-﻿namespace api.Models;
+﻿// This is a name of the module we create to later import in other files
+namespace api.Models;
+
+//This class is accecible from anywhere, and this is the blueprint for how data is created and stored
 public class Item
 {
-    public int Id { get; set; }
-    public string? Title { get; set; }
-    public bool IsComplete { get; set; }
+    public int Id { get; set; } // Id is a unique number assigned to a data
+    public string? Title { get; set; } // This is to edit or set to create or update data
+    public bool IsComplete { get; set; } // This is a boolean which only returns true of false
 }
--- Models/ItemDB.cs
+++ Models/ItemDB.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 namespace api.Models;
-public class ItemDB(DbContextOptions<ItemDB> options) : DbContext(options)
+
+// This is where the temporary database is connected using default options.
+public class ItemDB(DbContextOptions<ItemDB> options) : DbContext(options) // Inherits from Dbcontext
 {
-    public DbSet<Item> Item => Set<Item>();
+    public DbSet<Item> Item => Set<Item>(); // Sets a database using DbSet where Item is a blueprint.
+    // Item is where the every data in database is stored, they are taken from Item.cs
 }
